b400f941a2d1e73407ac3964618c99250b9f10b9
Victor Muryn <muryn.pn@ucu.edu.ua>
2023-02-08 09:50:06 +0200

    merged columns

--- puzzle.py
+++ puzzle.py
@@ -100,6 +100,26 @@ def check_cells(board):
         
     return True
 
+def columns_check(board: list[str]) -> bool:
+    '''
+    Transform the board and then check it in the check_row fnction
+    >>> column_check([\
+        "**** ****",\
+        "***1 ****",\
+        "**  3****",\
+        "* 4 1****",\
+        "     9 5 ",\
+        " 6  83  *",\
+        "3   1  **",\
+        "  8  2***",\
+        "  2  ****"\
+    ])
+    False
+    '''
+    columns = [''.join([el[i] for el in board]) for i in range(len(board[0]))]
+    return check_rows(columns)
+
+
 def validate_board(board: list[str]) -> bool:
     return False
 
