618cc5eb83b6dd9ed76e9ee63802594b30b27ef4
Havryliuk17 <havryliuk.pn@ucu.edu.ua>
2023-02-08 09:32:20 +0200

    test text

---
+++ puzzle.py
@@ -0,0 +1,22 @@
+def colomn_check(board: list[str]) -> bool:
+    '''
+    Transform the board and then check it in the check_row fnction
+    >>> colomn_check([\
+        "**** ****",\
+        "***1 ****",\
+        "**  3****",\
+        "* 4 1****",\
+        "     9 5 ",\
+        " 6  83  *",\
+        "3   1  **",\
+        "  8  2***",\
+        "  2  ****"\
+    ])
+    False
+    '''
+    colomns = [''.join([el[i] for el in board]) for i in range(len(board[0]))]
+    return check_row(colomns)
+
+if __name__ == "__main__":
+    import doctest
+    print(doctest.testmod())
