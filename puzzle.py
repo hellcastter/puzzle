be8fddf4f661f4274499176c3b9d4036a63c6de8
Victor Muryn <muryn.pn@ucu.edu.ua>
2023-02-08 09:30:58 +0200

    decomposited

--- puzzle.py
+++ puzzle.py
@@ -1,19 +1,65 @@
-def check_row(board):
-    """
-    >>> check_row([  "**** ****","***1 ****", "**  3****", "* 4 1****", "     9 5 ", " 6  83  *", "3   1  **","  8  2***","  2  ****"])
+def check_row(line: str) -> bool:
+    """Check wheather in row only unique nums
+
+    Args:
+        line (str): board line
+
+    Returns:
+        bool: is valid
+    
+    >>> check_row("**** ****")
     True
-    >>> check_row([  "**** ****","***1 ****", "**  3****", "* 4 *****", "     9 5 ", " 6  83  *", "3   1  **","  8  2***","  2  ****"])
+    >>> check_row("* 4 1****")
     True
-    >>> check_row([  "**** ****","**11 ****", "**  3****", "* 4 *****", "     9 5 ", " 6  83  *", "3   1  **","  8  2***","  2  ****"])
+    >>> check_row("44** ****")
     False
     """
-    numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-    result = True
-    for i in board:
-        for j in i:
-            if j in numbers and i.count(j) != 1:
-                result = False
-    return result
+    for i in line:
+        if i.isnumeric() and line.count(i) != 1:
+            return False
+            
+    return True
+
+def check_rows(board):
+    """
+    >>> check_rows([ \
+        "**** ****", \
+        "***1 ****", \
+        "**  3****", \
+        "* 4 1****", \
+        "     9 5 ", \
+        " 6  83  *", \
+        "3   1  **", \
+        "  8  2***", \
+        "  2  ****" \
+    ])
+    True
+    >>> check_rows([ \
+        "**** ****", \
+        "***1 ****", \
+        "**  3****", \
+        "* 4 *****", \
+        "     9 5 ", \
+        " 6  83  *", \
+        "3   1  **", \
+        "  8  2***", \
+        "  2  ****" \
+    ])
+    True
+    >>> check_rows([ \
+        "**** ****", \
+        "**11 ****", \
+        "**  3****", \
+        "* 4 *****", \
+        "     9 5 ", \
+        " 6  83  *", \
+        "3   1  **", \
+        "  8  2***", \
+        "  2  ****" \
+    ])
+    False
+    """
+    return all([check_row(line) for line in board])
 
 if __name__ == "__main__":
   import doctest
